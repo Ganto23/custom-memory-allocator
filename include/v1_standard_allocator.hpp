bf02a4b7069b072d94e800bdcdadba1f7e34be4a
Godfrey Antomarlin <godfrey.antomarlin@gmail.com>
2026-03-20 19:47:33 +0000

    Added standard Heap Allocator for v1 as baseline

---
+++ include/v1_standard_allocator.hpp
@@ -0,0 +1,11 @@
+template <typename T>
+class HeapAllocator {
+public:
+    template <typename... Args>
+    T* allocate(Args&&... args){
+        return new T(std::forward<Args>(args)...);
+    }
+    void deallocate(T* ptr) {
+        delete ptr;
+    }
+};
